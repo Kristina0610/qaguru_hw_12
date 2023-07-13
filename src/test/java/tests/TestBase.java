3a51f65b359e32d31b040352797751aa072e8fe1
Kristina0610 <gaevskaya-2011@mail.ru>
2023-07-13 17:35:52 +0300

    Configuration.browser = "firefox"

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,11 +13,11 @@ public class TestBase {
   static void beforeAll() {
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.browserSize = "1520x1080";
-    //Configuration.browser = "firefox";
+    Configuration.browser = "firefox";
     //Configuration.timeout = 5000;
     //Эта настройка упрощает загрузку сайтов,
     //которые зависят от плохо грузящихся ресурсов
-    Configuration.pageLoadStrategy = "eager";
+    //Configuration.pageLoadStrategy = "eager";
     //Configuration.holdBrowserOpen = true;
   }
 
