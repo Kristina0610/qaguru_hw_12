07d1eafff29f9d17dc7a2a674f0d78c63a11e88e
Kristina0610 <gaevskaya-2011@mail.ru>
2023-07-13 19:29:18 +0300

    added @Step

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -48,6 +48,7 @@ public class RegistrationPage {
     return this;
   }
 
+  @Step("Удаляем footer")
   public RegistrationPage removeFooter() {
 
     executeJavaScript("$('footer').remove()");
@@ -55,6 +56,7 @@ public class RegistrationPage {
     return this;
   }
 
+  @Step("Удаляем окно с рекламой")
   public RegistrationPage removeBanners() {
 
     executeJavaScript("$('#fixedban').remove()");
@@ -62,30 +64,35 @@ public class RegistrationPage {
     return this;
   }
 
+  @Step("Заполняем имя")
   public RegistrationPage setFirstNameInput(String value) {
     firstNameInput.setValue(value);
 
     return this;
   }
 
+  @Step("Заполняем фамилию")
   public RegistrationPage setLastNameInput(String value) {
     lastNameInput.setValue(value);
 
     return this;
   }
 
+  @Step("Заполняем Email")
   public RegistrationPage setUserEmailInput(String value) {
     userEmailInput.setValue(value);
 
     return this;
   }
 
+  @Step("Заполняем пол")
   public RegistrationPage setGender(String value) {
     gender.$(byText(value)).click();
 
     return this;
   }
 
+  @Step("Заполняем номер телефона")
   public RegistrationPage setNumber(String value) {
     userNumber.setValue(value);
 
@@ -93,6 +100,7 @@ public class RegistrationPage {
   }
 
 
+  @Step("Заполняем дату рождения")
   public RegistrationPage setBirthDate(String day, String month, String year) {
     dateOfBirthInput.click();
     calendarComponent.setDate(day, month, year);
@@ -100,30 +108,35 @@ public class RegistrationPage {
     return this;
   }
 
+  @Step("Выбираем хобби")
   public RegistrationPage setHobby(String value) {
     hobby.$(byText(value)).click();
 
     return this;
   }
 
+  @Step("Заполняем предмет")
   public RegistrationPage setSubjectInput(String value) {
     subjectInput.setValue(value).pressEnter();
 
     return this;
   }
 
+  @Step("Загружаем файл")
   public RegistrationPage uploadFile() {
     fileUpload.uploadFile(new File("src/test/resources/screenshot_1.png"));
 
     return this;
   }
 
+  @Step("Заполняем адрес")
   public RegistrationPage setCurrentAddress(String value) {
     currentAddress.setValue(value);
 
     return this;
   }
 
+  @Step("Выбираем из списка штат")
   public RegistrationPage setState(String value) {
     state.click();
     $(byText(value)).click();
@@ -131,7 +144,7 @@ public class RegistrationPage {
     return this;
   }
 
-
+  @Step("Выбираем из списка город {value}")
   public RegistrationPage setCity(String value) {
     city.click();
     $(byText(value)).click();
@@ -139,17 +152,20 @@ public class RegistrationPage {
     return this;
   }
 
+  @Step("Нажимаем кнопку Button")
   public RegistrationPage clickButton() {
     button.click();
 
     return this;
   }
 
+  @Step("Проверяем, что все параметры заполнены верно")
   public RegistrationPage verifyRegistrationResultsModalAppears() {
     resultsModal.verifyModalAppears();
     return this;
   }
 
+  @Step("Проверяем отображение модального окна")
   public RegistrationPage verifyResult(String key, String value) {
     resultsModal.verifyResult(key, value);
 
