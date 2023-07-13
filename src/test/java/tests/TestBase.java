d968ccef0fd8b76507d47d515831a1398a6b7fca
Kristina0610 <gaevskaya-2011@mail.ru>
2023-07-13 17:32:12 +0300

    added allure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,10 @@ package tests;
 
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -10,12 +13,17 @@ public class TestBase {
   static void beforeAll() {
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.browserSize = "1520x1080";
-    Configuration.browser = "firefox";
+    //Configuration.browser = "firefox";
     //Configuration.timeout = 5000;
     //Эта настройка упрощает загрузку сайтов,
     //которые зависят от плохо грузящихся ресурсов
     Configuration.pageLoadStrategy = "eager";
     //Configuration.holdBrowserOpen = true;
+  }
+
+  @BeforeEach
+  void addListener() {
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
   }
 }
 
