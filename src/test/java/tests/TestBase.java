b904c892191f4fb14ca742b390fd0bd0daabaf0a
Kristina0610 <gaevskaya-2011@mail.ru>
2023-07-13 19:10:27 +0300

    added listner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,12 +16,9 @@ public class TestBase {
   static void beforeAll() {
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.browserSize = "1520x1080";
-    //Configuration.browser = "firefox";
-    //Configuration.timeout = 5000;
     //Эта настройка упрощает загрузку сайтов,
     //которые зависят от плохо грузящихся ресурсов
     Configuration.pageLoadStrategy = "eager";
-    //Configuration.holdBrowserOpen = true;
     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
